be4580d4a239e19f205ec64f84a766e841adf2e4
dycy21 <openmevi@gmail.com>
2025-11-07 16:25:26 +0530

    single dockerfile

--- q-a_app/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# File: startup.sh
-
-# Start PHP-FPM in the background (-D)
-/usr/local/sbin/php-fpm -D
-
-# Start Nginx in the foreground (required by Docker)
-exec nginx -g "daemon off;"
